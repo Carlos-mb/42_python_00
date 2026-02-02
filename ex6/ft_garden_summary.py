f98889f61a11f3c6f0aade0c6e96b821506ebd27
Carlos Melero <carlos@carlosmelero.com>
2026-02-02 15:50:44 +0100

    Final

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    garden_name: str = input("Enter garden name: ")
+    plants: str = input("Enter number of plants: ")
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {plants}")
+    print("Status: Growing well!")
